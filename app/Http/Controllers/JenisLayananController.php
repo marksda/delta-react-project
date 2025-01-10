15f6cfc832be67ce51fea6f76313ba26aa2f6ec1
marksda <vlack123@gmail.com>
2025-01-10 16:54:06 +0700

    cso

---
+++ app/Http/Controllers/JenisLayananController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreJenisLayananRequest;
+use App\Http\Requests\UpdateJenisLayananRequest;
+use App\Models\JenisLayanan;
+
+class JenisLayananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJenisLayananRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JenisLayanan $jenisLayanan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JenisLayanan $jenisLayanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJenisLayananRequest $request, JenisLayanan $jenisLayanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JenisLayanan $jenisLayanan)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RuangLayananController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRuangLayananRequest;
+use App\Http\Requests\UpdateRuangLayananRequest;
+use App\Models\RuangLayanan;
+
+class RuangLayananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRuangLayananRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RuangLayanan $ruangLayanan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RuangLayanan $ruangLayanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRuangLayananRequest $request, RuangLayanan $ruangLayanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RuangLayanan $ruangLayanan)
+    {
+        //
+    }
+}
